a84c08ef44f6f42cf8363cccad919c147d0733b6
Dagm Behailu <dagmbehailu@lawn-143-215-53-11.lawn.gatech.edu>
2018-11-03 23:39:02 -0400

    working boiler

---
+++ GTOpportunistsTHEAPP/app/src/main/java/com/gitmad/gtopportuniststheapp/PhotoGalleryActivity.java
@@ -0,0 +1,114 @@
+package com.gitmad.gtopportuniststheapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.GridView;
+
+
+public class PhotoGalleryActivity extends AppCompatActivity {
+
+    private GridView photoGrid;
+    private BottomNavigationView navBar;
+    private MenuItem home;
+    private MenuItem camera;
+    private MenuItem profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_gallery);
+
+
+
+//        GridView photoGrid = (GridView) findViewById(R.id.gridview);
+//
+//        //TODO 1: Set the adapter for the photo grid to the ImageAdapter class
+//
+//        photoGrid.setAdapter(new ImageAdapter(getApplicationContext()));
+//        photoGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View v,
+//                                    int position, long id) {
+//                Toast.makeText(PhotoGalleryActivity.this, "" + position,
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        navBar = findViewById(R.id.navigation);
+
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                System.out.println(item.toString());
+
+                //TODO 2: Based on the item that was selected (string representation of "item", change
+                //The activity to a new activity for the profile or camera.
+
+                return true;
+            }
+        });
+    }
+
+
+//    public class ImageAdapter extends BaseAdapter {
+//        private Context mContext;
+//
+//        public ImageAdapter(Context c) {
+//            mContext = c;
+//        }
+//
+//        public int getCount() {
+//            return mThumbIds.length;
+//        }
+//
+//        public Object getItem(int position) {
+//            return null;
+//        }
+//
+//        public long getItemId(int position) {
+//            return 0;
+//        }
+//
+//        // create a new ImageView for each item referenced by the Adapter
+////        public View getView(int position, View convertView, ViewGroup parent) {
+////            ImageView imageView;
+////            if (convertView == null) {
+////                // if it's not recycled, initialize some attributes
+////                imageView = new ImageView(mContext);
+////
+////                //TODO 2: Set the width and height that you want for the images in the grid
+////                imageView.setLayoutParams(new ViewGroup.LayoutParams(23, 51));
+////
+////                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+////                imageView.setPadding(8, 8, 8, 8);
+////            } else {
+////                imageView = (ImageView) convertView;
+////            }
+////
+////
+////            //TODO 3: Set the resource for this image view to the passed in position in
+////            //mThumbIds
+////
+////            return imageView;
+////
+////        }
+//
+//        // references to our images
+//        private Integer[] mThumbIds = {
+//                R.drawable.sample_2, R.drawable.sample_3,
+//                R.drawable.sample_4, R.drawable.sample_5,
+//                R.drawable.sample_6, R.drawable.sample_7,
+//                R.drawable.sample_0, R.drawable.sample_1,
+//                R.drawable.sample_2, R.drawable.sample_3,
+//                R.drawable.sample_4, R.drawable.sample_5,
+//                R.drawable.sample_6, R.drawable.sample_7,
+//                R.drawable.sample_0, R.drawable.sample_1,
+//                R.drawable.sample_2, R.drawable.sample_3,
+//                R.drawable.sample_4, R.drawable.sample_5,
+//                R.drawable.sample_6, R.drawable.sample_7
+//        };
+//    }
+
+}
